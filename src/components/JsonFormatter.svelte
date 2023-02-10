6cc9ed48e0100b1d3b2dbc7abba7f3881f59176e
taq-fujihara <taq.fujihara@gmail.com>
2023-02-10 14:25:28 +0900

    Simple JSON formatter

---
+++ src/components/JsonFormatter.svelte
@@ -0,0 +1,37 @@
+<script>
+  let input = '{"foo":"bar"}'
+  let errorMessage = null
+
+  function format() {
+    try {
+      input = JSON.stringify(JSON.parse(input), null, 2)
+      errorMessage = null
+    } catch (error) {
+      errorMessage = error.message
+    }
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(input)
+  }
+</script>
+
+{#if errorMessage}
+  <div class="error">
+    {errorMessage}
+  </div>
+{/if}
+
+<div>
+  <textarea bind:value={input} cols="30" rows="10" />
+</div>
+<div>
+  <button on:click={format}>Format</button>
+  <button on:click={copyToClipboard}>Copy</button>
+</div>
+
+<style>
+  .error {
+    color: red;
+  }
+</style>
