b9340d069cd35b2a8617b5234b1ea63480f98c71
taq-fujihara <taq.fujihara@gmail.com>
2023-02-10 14:32:52 +0900

    add GitHub Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
-});
+  integrations: [svelte()],
+  site: 'https://taq-fujihara.github.io',
+  base: '/astro-starter',
+})
